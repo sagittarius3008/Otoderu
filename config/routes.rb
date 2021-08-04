8680f9896223c1b2264906ee494c7ccdf8726cc7
sagittarius3008 <sagittarius30008@gmail.com>
2021-08-04 01:38:28 +0000

    [Add]devise/top/about

--- config/routes.rb
+++ config/routes.rb
@@ -4,26 +4,25 @@ Rails.application.routes.draw do
     passwords:     'orchestras/passwords',
     registrations: 'orchestras/registrations'
   }
-  
+
   devise_for :members, controllers: {
     sessions:      'members/sessions',
     passwords:     'members/passwords',
     registrations: 'members/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
-  
-  namespace :members do
+
     resources :members
     resources :attendances
     resources :belongings
+
+  namespace :groups do
+    resources :orchestras do
+      resources :instruments, only:[:create, :index, :update]
+    end
   end
-  
-  namespace :orchestras do
-    resources :orchestras
-    resources :instruments, only:[:create, :index, :update]
-  end
-  
+
 end
