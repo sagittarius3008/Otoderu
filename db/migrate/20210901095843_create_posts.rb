0bfbcf2e8e367390f763b2fdd055041953af3527
sagittarius3008 <sagittarius30008@gmail.com>
2021-09-01 15:07:34 +0000

    [Add]native language

---
+++ db/migrate/20210901095843_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.integer :orchestra_id, null: false
+      t.text :body, null: false
+      t.decimal :score, precision: 5, scale: 3
+      t.timestamps
+    end
+  end
+end
